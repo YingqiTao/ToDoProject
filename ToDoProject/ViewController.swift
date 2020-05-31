edc54d648abd99d410781ed9c8dc36c688d8cd8d
wendytao <wendytao@umich.edu>
2020-05-30 21:25:35 -0400

    Initial Commit

---
+++ ToDoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoProject
+//
+//  Created by YingQi Tao on 5/30/20.
+//  Copyright © 2020 YingQi Tao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
