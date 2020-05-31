552fb41c51993802d1aa8327a1113b354e630579
wendytao <yingqitao2000@gmail.com>
2020-05-31 15:20:02 -0400

    Finished Iteration 0

---
+++ ToDoProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoProject
+//
+//  Created by YingQi Tao on 5/31/20.
+//  Copyright © 2020 YingQi Tao. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
